4674748d36b9213f37be96d92dbf0206a7afda30
khokher-poonam <khokherpoonam13@gmail.com>
2020-06-22 21:36:05 -0400

    AIMedicalInfoPage added

---
+++ src/test/java/co/pragra/learning/selenium/framework/pages/AIMedicalInfoPage.java
@@ -0,0 +1,19 @@
+package co.pragra.learning.selenium.framework.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AIMedicalInfoPage {
+    WebDriver driver;
+    @FindBy(xpath = "//input[@type='name' and @placeholder='Parent/Agent Email']")
+    WebElement ParentAgent;
+
+
+    public AIMedicalInfoPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+}
+
